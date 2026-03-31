9bde6e1e46144ff7e4635935b4ae5bda2a53f647
Akendra Varma Nellipudi <akendravarma45@gmail.com>
2026-03-31 16:11:08 +0530

    Add files via upload

---
+++ Array/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main (String[]args){
+        int arr[] = {1,2,3,4};
+
+        for (int  i = 0 ; i<=3;i++){
+            System.out.println(arr[i]);
+        }
+        for(int i =3;i>=0;i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
